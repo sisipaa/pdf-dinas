6a2afbe759ad18c613d928ab004bd061babf0af1
Syifa Qisthi <syifaqr04@gmail.com>
2026-05-14 00:02:51 +0700

    add pdf.blade dalam negri

--- resources/views/trips/dalam-negeri/pdf.blade.php
+++ resources/views/trips/dalam-negeri/pdf.blade.php
@@ -12,7 +12,7 @@
         font-family: "Times New Roman", Times, serif;
         font-size: 11pt;
         color: #000;
-        line-height: 1.2;
+        line-height: 1.4;
     }
 
     table {
@@ -54,6 +54,7 @@
         text-align: center;
         font-weight: bold;
         font-size: 13pt;
+        margin-bottom: 10px;
     }
 
     .page-break {
@@ -68,4 +69,57 @@
         border: 0;
         border-top: 1px solid #000;
         margin: 5px 0;
+    }
+
+    .mt-20 {
+        margin-top: 20px;
+    }
+
+    .mt-30 {
+        margin-top: 30px;
+    }
+
+    .signature {
+        width: 250px;
+        float: right;
+        text-align: center;
+    }
+
+    .logo {
+        width: 80px;
+    }
+</style>
+</head>
+
+<body>
+
+{{-- ================= SPD ================= --}}
+@include('trips.dalam-negeri.partials.spd', ['trip' => $trip])
+
+<div class="page-break"></div>
+
+{{-- ================= RINCIAN ================= --}}
+@include('trips.dalam-negeri.partials.rincian', ['trip' => $trip])
+
+<div class="page-break"></div>
+
+{{-- ================= KWITANSI ================= --}}
+@include('trips.dalam-negeri.partials.kwitansi', ['trip' => $trip])
+
+<div class="page-break"></div>
+
+{{-- ================= RIIL ================= --}}
+@include('trips.dalam-negeri.partials.riil', ['trip' => $trip])
+
+<div class="page-break"></div>
+
+{{-- ================= NOMINATIF ================= --}}
+@include('trips.dalam-negeri.partials.nominatif', ['trip' => $trip])
+
+<div class="page-break"></div>
+
+{{-- ================= PERJALANAN ================= --}}
+@include('trips.dalam-negeri.partials.perjalanan', ['trip' => $trip])
+
+</body>
 </html>
